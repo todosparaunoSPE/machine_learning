cc505b65ec25b23ca4df9590366d41bb83d9d5cc
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-12-13 10:59:01 -0600

    Update 1_Modelos_supervisados.py

--- pages/1_Modelos_supervisados.py
+++ pages/1_Modelos_supervisados.py
@@ -18,6 +18,22 @@ from itertools import combinations
 import time
 import plotly.express as px
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 class ModelAnalysis:
     def __init__(self, input_df, model_type=None, target_column='Class', seed=None):
         self.input_df = input_df
